8da0d22e257f877502808e84a05cf521b9b4b28d
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-17 16:00:44 +0300

    create AdminCategoryBlueprint

---
+++ src/api/lingany_api/blueprints/admin/admin_category_blueprint.py
@@ -0,0 +1,27 @@
+import logging
+
+from flask import Blueprint, request, Response, json
+from injector import inject, singleton
+
+from lingany_api.blueprints.base_blueprint import BaseBlueprint
+
+
+@singleton
+class AdminCategoryBlueprint(BaseBlueprint):
+
+    @inject
+    def __init__(self) -> None:
+        super().__init__()
+
+    @property
+    def _name(self) -> str:
+        return 'admin_category'
+
+    def _create_blueprint(self) -> Blueprint:
+        blueprint = Blueprint(self._name, __name__)
+
+        @blueprint.route('/', methods=['POST'])
+        def _add():
+            return self._return_msg(f'name: {self._name}')
+
+        return blueprint
