a7dbcecce8227b8527afbbec4b50135403a48e46
pitikdmitry <pitik.dmitry@mail.ru>
2018-03-18 23:16:42 +0300

    add language converter

--- src/api/lingany_api/services/language_service.py
+++ src/api/lingany_api/services/language_service.py
@@ -2,27 +2,20 @@ from injector import singleton, inject
 from typing import Optional, List
 from uuid import UUID
 
-from sqlutils import AbstractExpandSet, Service
-from lingany_api import cache
+from lingany_api.converters.language_converter import LanguageConverter
 from lingany_api.models.language import Language
-from lingany_api.persistance.dto.language_dto import LanguageDTO
 from lingany_api.persistance.repositories.language_repository import LanguageRepository
 
 
 @singleton
-class LanguageService(Service[Language, LanguageDTO, LanguageRepository]):
+class LanguageService:
 
     @inject
     def __init__(self, repo: LanguageRepository) -> None:
-        super().__init__(repo)
-        # self._converter = LanguageConverter()
-
-    # def _convert(self, entity: UserDTO, expand: AbstractExpandSet) -> Optional[User]:
-    #     if not entity:
-    #         return None
-    #
-    #     user = self._converter.convert(entity)
-    #     return user
+        self._repo = repo
+        self._converter = LanguageConverter()
 
     def get_by_id(self, uid: UUID) -> Optional[Language]:
-        return super().get_by_id(uid)
+        language_dto = self._repo.get_by_id(uid)
+        language = self._converter.convert(language_dto)
+        return language
