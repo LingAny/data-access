b958838b6c5bac007d379cb4a04e6f9a539796a0
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-18 21:02:56 +0300

    get list of files

--- lib/loader/database_loader/load_db.py
+++ lib/loader/database_loader/load_db.py
@@ -1,5 +1,7 @@
 import os
 
+from os.path import isfile, join
+
 from sqlutils import EnvDataContextFactory
 
 
@@ -7,6 +9,14 @@ def main():
     print('MAIN')
     path = os.environ.get('DATABASE')
     context = EnvDataContextFactory().create_data_context()
+
+    print("path: " + path)
+    files = get_files(path + "/structure/dict")
+    print("files: ", files)
+
+
+def get_files(path):
+    return [f for f in os.listdir(path) if isfile(join(path, f))]
 
 
 if __name__ == '__main__':
