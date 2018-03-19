34e5b1163acdc8148a81cc72b625f368391b9920
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-19 19:55:50 +0300

    update Category

--- manage/data_manager/manager/models/category.py
+++ manage/data_manager/manager/models/category.py
@@ -1,3 +1,16 @@
+from typing import Set
+
+
 class Category(object):
 
-    pass
+    def __init__(self, title: str, values: Set[str]) -> None:
+        self._title = title
+        self._values = values
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def values(self) -> Set[str]:
+        return self._values
