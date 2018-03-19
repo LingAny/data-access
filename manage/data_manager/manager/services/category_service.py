cd91dcb60bd5c1b97989291da7809177cb408b9e
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-19 20:08:52 +0300

    create get_supported_categories

--- manage/data_manager/manager/services/category_service.py
+++ manage/data_manager/manager/services/category_service.py
@@ -1,10 +1,23 @@
+import logging
 from typing import List
 
 from manager.models.category import Category
+from manager.services.reader import FolderReader, LineReader
 
 
 class CategoryService(object):
 
     @staticmethod
-    def get_supported_categories() -> List[Category]:
-        pass
+    def get_supported_categories(path: str) -> List[Category]:
+        files = FolderReader.get_files(path)
+        categories = []
+        for filename in files:
+            logging.info(f'load category from {filename}')
+            data = LineReader.read(filename)
+            if len(data) < 2:
+                continue
+            title = data[0]
+            values = set(data[1:])
+            categories.append(Category(title=title, values=values))
+
+        return categories
--- manage/data_manager/manager/services/reader.py
+++ manage/data_manager/manager/services/reader.py
@@ -17,8 +17,26 @@ class CSVReader(object):
             return data
 
 
+class LineReader(object):
+
+    @staticmethod
+    def read(filename: str) -> List[Any]:
+        with open(filename, 'r') as file:
+            data = []
+            for line in file:
+                line = line.strip()
+                if line == '':
+                    continue
+                data.append(line)
+            return data
+
+
 class FolderReader(object):
 
     @staticmethod
     def get_files(path: str) -> List[str]:
         return [join(path, f) for f in os.listdir(path) if isfile(join(path, f))]
+
+    @staticmethod
+    def get_names(path: str) -> List[str]:
+        return [f for f in os.listdir(path) if isfile(join(path, f))]
