e20543cd4e6c3aa22f7f8f51dc17892d42a5a3a4
ZubAnt <ya.zubarevanton@yandex.ru>
2018-03-19 19:51:46 +0300

    create CategoryService

---
+++ manage/data_manager/manager/services/category_service.py
@@ -0,0 +1,10 @@
+from typing import List
+
+from manager.models.category import Category
+
+
+class CategoryService(object):
+
+    @staticmethod
+    def get_supported_categories() -> List[Category]:
+        pass
