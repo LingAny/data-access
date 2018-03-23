13732df3faba115098c32c75a51176d6383dfc3d
Dmitry Pitik <pitik.dmitry@mail.ru>
2018-03-23 13:33:20 +0300

    trainings tests without add

--- src/tests/lingany_api_tests/training/training_tests.py
+++ src/tests/lingany_api_tests/training/training_tests.py
@@ -16,39 +16,38 @@ class TrainingTestCase(unittest.TestCase):
         cls._stub = TrainingStub()
         cls._category_stub = CategoryStub()
 
-    def test_create(self):
+    def test_fail_create(self):
         category_id = self._category_stub.get_instance()['id']
         response, sut = self._stub.create(category_id=category_id)
-        self.assertEqual(201, response.status_code)
+        self.assertIn(response.status_code, [404, 405])
+
+    def test_fail_delete(self):
+        sut = self._stub.get_instance()
+        response = self._stub.delete(instance_id=sut['id'])
+        self.assertIn(response.status_code, [404, 405])
 
     def test_get_by_id(self):
-        response, sut = self._stub.create()
-        self.assertEqual(201, response.status_code)
+        sut = self._stub.get_instance()
         response, obj = self._stub.get_by_id(sut['id'])
         self.assertEqual(200, response.status_code)
         self._check(obj, sut)
 
     def test_get_all(self):
-        response, sut = self._stub.create()
-        self.assertEqual(201, response.status_code)
         response, list_obj = self._stub.get_all()
         self.assertEqual(200, response.status_code)
         self.assertGreater(len(list_obj), 0)
 
     def test_get_trainings_for_categories(self):
         category_id = self._category_stub.get_instance()['id']
-        response, sut = self._stub.create(category_id=category_id)
-        self.assertEqual(201, response.status_code)
 
-        response, obj = self._stub.get_trainings_for_categories(category_id)
+        response, trainings = self._stub.get_trainings_for_categories(category_id)
         self.assertEqual(200, response.status_code)
-        self._check(obj[0], sut)
-
-    def tearDown(self):
-        self._stub.clear()
+        self.assertGreater(len(trainings), 0)
 
     def _check(self, obj: Dict[str, Any], sut: Dict[str, Any]):
-        self.assertEqual(sut['id'], obj['id'])
-        self.assertIn('href', obj)
+        self.assertEqual(obj['id'], sut['id'])
+        self.assertEqual(obj['href'], sut['href'])
         self.assertEqual(sut['nativeWord'], obj['nativeWord'])
         self.assertEqual(sut['foreignWord'], obj['foreignWord'])
+        self.assertEqual(obj['category']['id'], sut['category']['id'])
+        self.assertEqual(obj['category']['href'], sut['category']['href'])
